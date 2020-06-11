173866c67b1d0017a070aabeeabf39f9e18d5fd5
tscotce <tatyana.celovsky@gmail.com>
2020-06-11 15:52:03 -0400

    Added Home view and root route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :students
   resources :workout_classes
   resources :instructors
+  root 'welcome#home'
   # resources :workout_studios
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
