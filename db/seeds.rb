fb31ddbc3e2d8ce6d89748f8ce5943b38e07a79c
tscotce <tatyana.celovsky@gmail.com>
2020-06-11 18:19:10 -0400

    Updated seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,49 +7,33 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 DATA = {
-  :workout_studio_keys =>
-    ["name", "address"],
-  :workout_studios => [
-    ["Sweat", "300 Broadway"]
-  ],
   :instructor_keys =>
-   ["name", "email", "password_digest", "workout_studio_id"],
+   ["name", "email", "password_digest"],
   :instructors => [
-    ["Alice Park", "alice@sweat.com", "alicepassword", 1],
-    ["Sam Newmann", "sam@sweat.com", "sampassword", 1]
+    ["Alice Park", "alice@sweat.com", "alicepassword"],
+    ["Sam Newmann", "sam@sweat.com", "sampassword"]
   ],
   :workout_class_keys =>
-   ["name", "date", "time", "workout_studio_id", "instructor_id", "student_id"],
+   ["name", "date", "time", "instructor_id", "student_id"],
   :workout_classes => [
-    ["HIIT", "7/1/2020", "7:00", 1, 1, 1],
-    ["Cardio", "7/1/2020", "12:00", 1, 1, 2],
-    ["Upper Body", "7/5/2020", "8:00", 1, 2, 3],
-    ["Abs", "7/6/2020", "13:00", 1, 2, 2]
+    ["HIIT", "7/1/2020", "7:00", 1, 1],
+    ["Cardio", "7/1/2020", "12:00", 1, 2],
+    ["Upper Body", "7/5/2020", "8:00", 2, 3],
+    ["Abs", "7/6/2020", "13:00", 2, 2]
   ],
   :student_keys =>
-   ["name", "email", "password_digest", "workout_studio_id"],
+   ["name", "email", "password_digest"],
   :students => [
-    ["Jimmy", "jimmy@email.com", "jimmypassword", 1],
-    ["Kate", "kate@email.com", "katepassword", 1],
-    ["Alex", "alex@email.com", "alexpassword", 1]
+    ["Jimmy", "jimmy@email.com", "jimmypassword"],
+    ["Kate", "kate@email.com", "katepassword"],
+    ["Alex", "alex@email.com", "alexpassword"]
   ],
 }
 
 def main
-  make_workout_studios
   make_workout_classes
   make_instructors
   make_students
-end
-
-def make_workout_studios
-  DATA[:workout_studios].each do |workout_studio|
-    new_workout_studio = WorkoutStudio.new
-    workout_studio.each_with_index do |attribute, i|
-      new_workout_studio.send(DATA[:workout_studio_keys][i]+"=", attribute)
-    end
-    new_workout_studio.save
-  end
 end
 
 def make_workout_classes
