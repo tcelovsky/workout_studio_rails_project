1ff6137258652c71fc4d3e7831d858d25d4d911d
tscotce <tatyana.celovsky@gmail.com>
2020-06-11 17:19:06 -0400

    Started working on New form for workout classes.

--- app/controllers/workout_classes_controller.rb
+++ app/controllers/workout_classes_controller.rb
@@ -1,2 +1,48 @@
 class WorkoutClassesController < ApplicationController
+    before_action :set_workout_class, only: [:show, :edit, :update, :destroy]
+
+    # GET /workout_classes
+    def index
+      @workout_classes = WorkoutClass.all
+    end
+  
+    # GET /workout_classes/1
+    def show
+    end
+  
+    # GET /workout_classes/new
+    def new
+      @workout_class = WorkoutClass.new
+    end
+  
+    # GET /workout_classes/1/edit
+    def edit
+    end
+  
+    # POST /workout_classes
+    def create
+      @workout_class = WorkoutClass.create(workout_class_params)
+
+    end
+  
+    # PATCH/PUT /workout_classes/1
+    def update
+    end
+  
+    # DELETE /workout_classes/1
+    def destroy
+      @workout_class.destroy
+
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_workout_class
+        @workout_class = WorkoutClass.find(params[:id])
+      end
+  
+      # White list params.
+      def workout_class_params
+        params.require(:workout_class).permit(:name, :date, :time, :instructor_id, :workout_studio_id, :student_id)
+      end
 end
