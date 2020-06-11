72981febabf5fe9ed7e7243a9e31246c02332358
tscotce <tatyana.celovsky@gmail.com>
2020-06-11 15:34:10 -0400

    Added Welcome controller.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :students
   resources :workout_classes
   resources :instructors
-  resources :workout_studios
+  # resources :workout_studios
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
