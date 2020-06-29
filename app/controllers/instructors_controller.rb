9627658e5737e993ef646ae7992883b8aafaf2d4
tscotce <tatyana.celovsky@gmail.com>
2020-06-29 17:41:04 -0400

    Started working on instructors controller.

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,2 +1,16 @@
 class InstructorsController < ApplicationController
+    before_action :set_instructor, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @instructors = Instructor.all
+    end
+
+    private
+    def set_instructor
+        @instructor = Instructor.find_by(id: params[:id])
+    end
+
+    def instructor_params
+        params.require(:instructor).permit(:name, :email, :password, :password_confirmation, :admin)
+    end
 end
