5a225f207366c5cd184d32f4d34bd1af743f36dc
tscotce <tatyana.celovsky@gmail.com>
2020-07-06 16:37:24 -0400

    Updated workout classes form.

--- app/models/workout_class.rb
+++ app/models/workout_class.rb
@@ -8,4 +8,11 @@ class WorkoutClass < ApplicationRecord
     # validates :date, presence: true
     # validates :time, presence: true
 
+    def instructor_name=(name)
+		self.instructor = Instructor.find_or_create_by(name: name)
+	end
+
+	def instructor_name
+		self.instructor ? self.instructor.name : nil
+  end
 end
